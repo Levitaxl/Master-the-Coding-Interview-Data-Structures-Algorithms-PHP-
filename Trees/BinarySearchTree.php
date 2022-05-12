1e7862a6a485bc16bde237b9eb11c952e935cd51
levitaxl <hermessamuel10@gmail.com>
2022-05-12 14:24:50 -0400

    ADD- Carga de la funcion remove de un bst

--- Trees/BinarySearchTree.php
+++ Trees/BinarySearchTree.php
@@ -70,6 +70,98 @@ class BinarySearchTree {
         return false;
     }
     
+    public function remove($value){
+        if(!$this->root){
+            return false;
+        }
+
+        $currentNode= $this->root;
+        $parentNode= null;
+        while($currentNode){
+            if($value<$currentNode->data){
+                $parentNode= $currentNode;
+                $currentNode= $currentNode->left;
+            }
+
+            else if($value> $currentNode->data){
+                $parentNode=$currentNode;
+                $currentNode= $currentNode->right;
+            }
+            else if($value == $currentNode->data){
+                //We have a match.
+
+                //Option1: No right child:
+                if($currentNode->right==null){  
+                    if($parentNode== null){
+                        $this->root = $currentNode->left;
+                    }
+                    else{
+                        //if parent > current value, make current lef child a child of parent
+                        if($currentNode->data<$parentNode->data){
+                            $parentNode->left= $currentNode->left;
+                        }
+                        
+                        //if parent < current value, make left child a right child of parent
+                        else if($currentNode->data>$parentNode->data){
+                            $parentNode->right = $currentNode->left;
+                        }
+
+                    }
+                }
+                //Option 2: Right Child wich doesnt have a left child
+                else if($currentNode->right->left == null){
+                    $currentNode->right->left= $currentNode->left;
+                    if($parentNode== null){
+                        $this->root = $currentNode->right;
+                    }
+
+                    else {
+                        if($currentNode->data < $parentNode->data){
+                            $parentNode->left = $currentNode->right;
+                        }
+
+                        else if($currentNode->data > $parentNode->data){
+                            $parentNode->right = $currentNode->right;
+                        }
+                    }
+
+                }
+
+                //Option 3: Right child that has a left child
+                else{
+
+                    $leftmost = $currentNode->right->left;
+                    $leftmostParent = $currentNode->right;
+
+                    while($leftmost->left!==null){
+                        $leftmostParent=$leftmost;
+                        $leftmost=$leftmost->left;
+                    }
+
+                    //Parent's left subtree is no leftmost's right subtree
+                    $leftmostParent->left= $leftmostParent->right;
+                    $leftmost->left= $currentNode->left;
+                    $leftmost->right = $currentNode->right;
+
+
+                    if($parentNode==null){
+                        $this->root= $leftmost;
+                    }
+
+                    else{
+                        if($currentNode->data<$parentNode->data){
+                            $parentNode->left = $leftmost;
+                        }
+
+                        else if($currentNode->data> $parentNode->data){
+                            $parentNode->right= $leftmost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
 };
 
 
@@ -84,5 +176,19 @@ class BinarySearchTree {
     $tree->insert(15);
     $tree->insert(1);
     //print_r($tree->lookup(9)->left);
+
+    print_r($tree);
+
+    $tree->remove(9);
+    $tree->remove(170);
+    $tree->remove(1);
+    $tree->remove(20);
+    $tree->remove(15);
+
+    echo('<br>');
+    echo('<br>');
+    echo('<br>');
+
+    print_r($tree);
 
 ?>
